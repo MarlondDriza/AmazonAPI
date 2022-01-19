4c9effeebd5452dcfc57e0e0728c15967564bf29
MarlondDriza <dragon5trainer@outlook.com>
2022-01-19 09:22:37 +0800

    y

--- amazon-affiliate-api-master/sample/app.js
+++ amazon-affiliate-api-master/sample/app.js
@@ -39,38 +39,38 @@ app.get('/api/product/:asin', function (req, res) {
   });
 });
 
-// app.post('/api/cart/', function (req, res) {
-//   client.cartCreate({ items: [req.body] }).then(function(results) {
-//     carts[results.Cart.CartId] = results.Cart.HMAC;
-//     res.json(results);
-//   }).catch(function(err) {
-//     res.status(500).json(err);
-//   });
-// });
+app.post('/api/cart/', function (req, res) {
+  client.cartCreate({ items: [req.body] }).then(function(results) {
+    carts[results.Cart.CartId] = results.Cart.HMAC;
+    res.json(results);
+  }).catch(function(err) {
+    res.status(500).json(err);
+  });
+});
 
-// app.put('/api/cart/:cartId', function (req, res) {
-//   client.cartAdd({
-//     CartId: req.params.cartId,
-//     HMAC: carts[req.params.cartId],
-//     items: [req.body]
-//   }).then(function(results) {
-//     res.json(results);
-//   }).catch(function(err) {
-//     res.status(500).json(err);
-//   });
-// });
+app.put('/api/cart/:cartId', function (req, res) {
+  client.cartAdd({
+    CartId: req.params.cartId,
+    HMAC: carts[req.params.cartId],
+    items: [req.body]
+  }).then(function(results) {
+    res.json(results);
+  }).catch(function(err) {
+    res.status(500).json(err);
+  });
+});
 
-// app.get('/api/cart/:cartId', function (req, res) {
-//   client.cartGet({
-//     CartId: req.params.cartId,
-//     HMAC: carts[req.params.cartId]
-//   }).then(function(results) {
-//     res.json(results);
-//   }).catch(function(err) {
-//     res.status(500).json(err);
-//   });
-// });
+app.get('/api/cart/:cartId', function (req, res) {
+  client.cartGet({
+    CartId: req.params.cartId,
+    HMAC: carts[req.params.cartId]
+  }).then(function(results) {
+    res.json(results);
+  }).catch(function(err) {
+    res.status(500).json(err);
+  });
+});
 
-// app.listen(3000, function () {
-//   console.log('app listening on port 3000');
-// });
+app.listen(3000, function () {
+  console.log('app listening on port 3000');
+});
